c8f05c32b817206ce8eb5f82f0b1c0841abdaa19
Dani Raznikov <daniraznikov@google.com>
2019-05-12 15:28:25 +0300

    yo

--- pypyodbc.py
+++ pypyodbc.py
@@ -502,7 +502,7 @@ def UTF16_dec(buffer):
     #    last_match = raw.find("\x00\x00", last_match + 2)
     last_match = raw.find("\x00\x00")
     while last_match != -1 and last_match % 2 != 0:
-        last_match = raw.find("\x00\x00", last_match + 2)
+        last_match = raw.find("\x00\x00", last_match + 1)
 
     l = len(raw)
     if last_match != -1:
